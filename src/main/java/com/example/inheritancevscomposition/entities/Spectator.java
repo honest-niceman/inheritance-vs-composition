e193e1c9c8082d71c2bb57a47c0b41bbccf00d71
Georgii Vlasov <honest.niceman@gmail.com>
2022-01-11 11:47:07 +0400

    New Model

---
+++ src/main/java/com/example/inheritancevscomposition/entities/Spectator.java
@@ -0,0 +1,91 @@
+package com.example.inheritancevscomposition.entities;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "spectator")
+public class Spectator {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @ManyToMany
+    @JoinTable(name = "liked_articles",
+            joinColumns = @JoinColumn(name = "spectator_id"),
+            inverseJoinColumns = @JoinColumn(name = "articles_id"))
+    private List<Article> likedArticles = new ArrayList<>();
+
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "street")
+    private String street;
+
+    @Column(name = "building")
+    private String building;
+
+    public List<Article> getLikedArticles() {
+        return likedArticles;
+    }
+
+    public void setLikedArticles(List<Article> likedArticles) {
+        this.likedArticles = likedArticles;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getBuilding() {
+        return building;
+    }
+
+    public void setBuilding(String building) {
+        this.building = building;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
