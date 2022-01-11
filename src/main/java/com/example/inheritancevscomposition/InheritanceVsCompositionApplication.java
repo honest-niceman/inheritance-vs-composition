2c33f78503ef4e6adf49942299c85f0f7fd3da20
Georgii Vlasov <honest.niceman@gmail.com>
2022-01-11 10:30:52 +0400

    Init commit

---
+++ src/main/java/com/example/inheritancevscomposition/InheritanceVsCompositionApplication.java
@@ -0,0 +1,13 @@
+package com.example.inheritancevscomposition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InheritanceVsCompositionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InheritanceVsCompositionApplication.class, args);
+    }
+
+}
